22af60346efa3f70cb10135e95f87327c4bec9a1
javier <espindolajavier2013@gmail.com>
2025-05-30 07:09:44 -0300

    `Update ToggleDark component to use client-side state and cookies for theme management`

--- src/components/ToggleDark.tsx
+++ src/components/ToggleDark.tsx
@@ -1,13 +1,75 @@
-import Image from 'next/image'
-import React from 'react'
+"use client";
+
+import { useEffect, useState } from "react";
+
+import Image from "next/image";
+import React from "react";
 
 export default function ToggleDark() {
+  function getCookie(name: string): string | null {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop()?.split(";").shift() ?? null;
+    return null;
+  }
+
+  function setCookie(name: string, value: string) {
+    document.cookie = `${name}=${value};path=/;max-age=31536000`;
+  }
+
+  const [theme, setTheme] = useState<string | null>(null);
+
+  useEffect(() => {
+    const savedTheme = getCookie("theme");
+    if (savedTheme) {
+      setTheme(savedTheme);
+      document.documentElement.classList.add(savedTheme);
+    } else {
+      setTheme("light");
+      setCookie("theme", "light");
+    }
+  }, []);
+
+  const toggleTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+      document.documentElement.classList.remove("light");
+      document.documentElement.classList.add("dark");
+      setCookie("theme", "dark");
+    } else {
+      setTheme("light");
+      document.documentElement.classList.remove("dark");
+      document.documentElement.classList.add("light");
+      setCookie("theme", "light");
+    }
+  };
+
+  if (!theme) return null;
+
   return (
     <>
-    <button className='p-3 bg-blue-700 rounded-xl h-fit hover:bg-blue-600 cursor-pointer'>
-                <Image src="/assets/images/icon-moon.svg" alt="imagen luna" className='' width={20} height={20} />
-                <Image src="/assets/images/icon-sun.svg" alt="imagen sol" className='hidden' width={20} height={20} />
-                </button>
+      <button
+        onClick={toggleTheme}
+        className="p-3 bg-blue-50 dark:bg-blue-700 rounded-xl h-fit hover:bg-blue-600 cursor-pointer"
+      >
+        {theme === "light" ? (
+          <Image
+            src="/assets/images/icon-moon.svg"
+            alt="imagen luna"
+            className=""
+            width={20}
+            height={20}
+          />
+        ) : (
+          <Image
+            src="/assets/images/icon-sun.svg"
+            alt="imagen sol"
+            className=" "
+            width={20}
+            height={20}
+          />
+        )}
+      </button>
     </>
-  )
+  );
 }
