a4ccaa0e5ac8835b7090e457702a6f50adc51dc8
javier <espindolajavier2013@gmail.com>
2025-06-02 19:27:43 -0300

    `Refactor Home component layout and styles`

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,22 +45,32 @@ export default function Home() {
 
             {/* <!-- If you plan to use the JSON file to populate the data dynamically, you can delete the content below --> */}
 
-            <section>
-              <div>
-                <Image src="" alt="" />
-                <div>
-                  <h4>DevLens</h4>
-                  <p>
+            <section className="w-80 dark:bg-blue-900 bg-blue-100 p-5 rounded-2xl border border-blue-300 ">
+              <div className="flex gap-4 items-start">
+                <Image
+                  src="/assets/images/logo-devlens.svg"
+                  alt="devlens logo"
+                  width={50}
+                  height={50}
+                />
+                <div className="flex flex-col gap-2">
+                  <h4 className="font-bold">DevLens</h4>
+                  <p className="text-sm">
                     Quickly inspect page layouts and visualize element
                     boundaries.
                   </p>
                 </div>
               </div>
 
-              <div>
-                <button>Remove</button>
+              <div className="flex justify-between mt-6">
+                <button className="border border-blue-300 text-white hover:bg-red-900 rounded-2xl py-1 px-3">
+                  Remove
+                </button>
 
-                <button></button>
+                <label className="inline-flex items-center cursor-pointer">
+                  <input type="checkbox" value="" className="sr-only peer" />
+                  <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none   rounded-full peer dark:bg-blue-500 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-red-600 dark:peer-checked:bg-red-600"></div>
+                </label>
               </div>
             </section>
 
